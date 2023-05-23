418a9c3b21a5f388b64de33c9e480e2a6ac10c2a
Cody Lee Cochran <cody.l.cochran@gmail.com>
2023-05-23 21:46:03 +0000

    Add new file

---
+++ timestamp.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+timestamp="$CI_COMMIT_TIMESTAMP"
+if [[ "${timestamp}" == "" ]]; then timestamp="$(date --iso-8601=seconds)"; fi
+copyright_string="copyright: Copyright &copy; 2023 Matters of the Mind Therapy, LLC | Last updated: ${timestamp}"
+echo "[INFO] Replacing previous copyright line in mkdocs.yml with:"
+echo "       ${copyright_string}"
+while IFS=$'\n' read -r line; do
+  if [[ "${line}" =~ ^copyright: ]]; then
+    config_lines+=( "${copyright_string}" )
+  else
+    config_lines+=( "${line}" )
+  fi
+done < ./mkdocs.yml
+: > ./mkdocs.yml
+for config_line in "${config_lines[@]}"; do
+  echo "${config_line}" >> ./mkdocs.yml
+done
